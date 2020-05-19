aeac54fd0a4250661395aba15fe22f9fc93b5029
Alex <alex.grig.sur@gmail.com>
2020-05-19 16:26:02 +0300

    Update EpidemicEinstein.pl

--- EpidemicEinstein.pl
+++ EpidemicEinstein.pl
@@ -1,26 +1,56 @@
-%  событие, место, профессия, кол-во зараженных
 in_list([El|_],El).
 in_list([_|T],El):-in_list(T,El).
 
-pr_ein:-Event=[_,_,_,_],
-    in_list(Event,[funeral,italy,_,_]),
-    in_list(Event,[wedding,france,_,_]),
-    in_list(Event,[_,france,_,15]),
-    in_list(Event,[rest,china,_,_]),
-    in_list(Event,[conference,_,programmer,_]),
-    in_list(Event,[_,usa,_,1]),
-    in_list(Event,[_,_,official,45]),
-    in_list(Event,[_,_,banker,6]),
-    in_list(Event,[_,_,blogger,_]),
-    not(in_list(Event,[_,china,blogger,_])),
-    not(in_list(Event,[_,china,official,_])),
+/*
+Пятеро детей Алик, Боря, Витя, Лена и Даша приехали в лагерь из 5
+разных городов: Харькова, Умани, Полтавы, Славянска и Краматорска. Есть
+4 высказывания: 1) Если Алик не из Умани, то Боря из Краматорска. 2) Или
+Боря, или Витя приехали из Харькова. 3) Если Витя не из Славянска, то Лена
+приехала из Харькова. 4) Или Даша приехала из Умани, или Лена из
+Краматорска. Кто откуда приехал?
+*/
 
-    in_list(Event,[funeral,F1,F2,F3]),
-    in_list(Event,[wedding,S1,S2,S3]),
-    in_list(Event,[rest,T1,T2,T3]),
-    in_list(Event,[conference,Frth1,Frth2,Frth3]),
+iz4_predicate:-Childrens=[_,_,_,_,_],
+    in_list(Childrens,[alik,_]),
+    in_list(Childrens,[borya,_]),
+    in_list(Childrens,[vitya,_]),
+    in_list(Childrens,[lena,_]),
+    in_list(Childrens,[dasha,_]),
+    in_list(Childrens,[_,harkov]),
+    in_list(Childrens,[_,ymani]),
+    in_list(Childrens,[_,poltava]),
+    in_list(Childrens,[_,slavyansk]),
+    in_list(Childrens,[_,kramatorsk]),
+    %1
+    (
+      (
+         in_list(Childrens,[alik,ymani]);
+         in_list(Childrens,[borya,kramatorsk])
+       ),
+       %2
+       (
+         in_list(Childrens,[borya,harkov]);
+         in_list(Childrens,[vitya,harkov])
+       ),
+       %3
+       (
+         in_list(Childrens,[vitya,slavyansk]);
+         in_list(Childrens,[lena,harkov])
+       ),
+       %4
+       (
+         in_list(Childrens,[dasha,ymani]);
+         in_list(Childrens,[lena,kramatorsk])
+       )
+    ),
+    in_list(Childrens,[F1,harkov]),
+    in_list(Childrens,[F2,ymani]),
+    in_list(Childrens,[F3,poltava]),
+    in_list(Childrens,[F4,slavyansk]),
+    in_list(Childrens,[F5,kramatorsk]),
 
-    write("funeral "),write(F1),write(" "),write(F2),write(" "),write(F3),nl,
-    write("wedding "),write(S1),write(" "),write(S2),write(" "),write(S3),nl,
-    write("rest "),write(T1),write(" "),write(T2),write(" "),write(T3),nl,
-    write("conference "),write(Frth1),write(" "),write(Frth2),write(" "),write(Frth3), !.
+    write("Harkov"),write(" "),write(F1),nl,
+    write("Ymani"),write(" "),write(F2),nl,
+    write("Poltava"),write(" "),write(F3),nl,
+    write("Slavyansk"),write(" "),write(F4),nl,
+    write("Kramatorsk"),write(" "),write(F5),!.
